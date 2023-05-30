d67fe5fd221e1a73de1d83047d3af60f2f80d176
Brain Wang <82687988+Peaperfish@users.noreply.github.com>
2023-05-30 16:21:07 -0400

    added tower class

---
+++ Tower.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+public class Tower extends Actor {
+    
+    /**
+     * Create a tower that will shoot the bad guys
+     */
+    
+    // tower
+    public Tower() {
+        GreenfootImage image = getImage(); 
+        int newWidth = image.getWidth();
+        int newHeight = image.getHeight();
+        image.scale(60, 60);
+        setImage(image); 
+    }
+    
+    public void act() {
+        // Add your action code here.
+    }
+}
