7b4e0b49ded0d714d798df72d43cd10b74fdafaa
Brain Wang <82687988+Peaperfish@users.noreply.github.com>
2023-05-30 16:40:45 -0400

    added road class, and grass image for the road  (resized))

---
+++ Road.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * the road where the bad guy will be walking on
+ * 
+ * @author Brian Wang
+ * @version June 2023
+ */
+public class Road extends Actor
+{
+    /**
+     * Act - do whatever the Road wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public Road() {
+        getImage().scale(60, 60);
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
