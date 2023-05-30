3764b55720b9c48e0ff42a43a9c8e124e1ae247f
Brain Wang <82687988+Peaperfish@users.noreply.github.com>
2023-05-30 17:48:54 -0400

    created money display class

---
+++ MoneyDisplay.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MoneyyDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MoneyDisplay extends Actor
+{
+    /**
+     * label to show the amount of money 
+     */
+    
+    public MoneyDisplay() {
+        setImage(new GreenfootImage("", 20, Color.BLACK, Color.WHITE));
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
