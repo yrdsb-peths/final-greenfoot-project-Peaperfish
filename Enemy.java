eaa17286b30ef0f3ce506a1d356ab3936d082832
Brain Wang <82687988+Peaperfish@users.noreply.github.com>
2023-05-30 21:30:34 -0400

    "fixed" enrmy

--- Enemy.java
+++ Enemy.java
@@ -12,28 +12,28 @@ public class Enemy extends Actor {
     public void act() {
         move(2);
         
-        List<Road> road0 = getIntersectingObjects(Road.class);
+        List <Road> road0 = getObjectsAtOffset(-30, 0, Road.class);
         for (Road road : road0) {
             if (!road.straight && getRotation() == 90) {
                 setRotation(road.turn);
             }
         }
         
-        List<Road> road90 = getIntersectingObjects(Road.class);
+        List <Road> road90 = getObjectsAtOffset(0, -30, Road.class);
         for (Road road : road90) {
             if (!road.straight && getRotation() == 0) {
                 setRotation(road.turn);
             }
         }
         
-        List<Road> road180 = getIntersectingObjects(Road.class);
+        List <Road> road180 = getObjectsAtOffset(0, 30, Road.class);
         for (Road road : road180) {
             if (!road.straight && getRotation() == 270) {
                 setRotation(road.turn);
             }
         }
         
-        List<Road> road270 = getIntersectingObjects(Road.class);
+        List <Road> road270 = getObjectsAtOffset(30, 0, Road.class);
         for (Road road : road270) {
             if (!road.straight && getRotation() == 180) {
                 setRotation(road.turn);
