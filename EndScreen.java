f0b22a5970c76f80397820ee05c31e0b85a82eed
Brain Wang <82687988+Peaperfish@users.noreply.github.com>
2023-06-08 21:09:50 -0400

    endScreen half done

--- EndScreen.java
+++ EndScreen.java
@@ -17,5 +17,23 @@ public class EndScreen extends World
     {    
         // Create a new world with 840 x 600 cells with a cell size of 1 x 1 pixels.
         super(840, 600, 1); 
+        prepare();
     }
+    
+    private void prepare() {
+        // Display the game over message
+        showText("Game Over", getWidth() / 2, getHeight() / 2 - 50);
+
+        // Create the restart button and add it to the world
+        TryAgain tryAgain = new TryAgain();
+        addObject(tryAgain, getWidth() / 2, getHeight() / 2 + 50);
+    }
+    
+    
+    public void act() {
+        if (Greenfoot.mouseClicked(_obj_)) {
+            Greenfoot.setWorld(new MyWorld());
+        }
+    }
+    
 }
